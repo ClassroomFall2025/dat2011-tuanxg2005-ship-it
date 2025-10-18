97464ff4e8c834854133bc9fdbe02f96f92de712
tuatuan <tuanxg2005@tuanxg2005gmail.g2005gmailcom>
2025-10-18 07:34:28 +0700

    tuanlap5

--- sanpham.py
+++ sanpham.py
@@ -1,27 +1,35 @@
 class SanPham:
     def __init__(self,ten_san_pham,gia,giam_gia):
-        self.ten_san_pham = ten_san_pham
-        self.gia = gia
-        self.giam_gia = giam_gia
-    #def doc_giam_gia (self):
-    #    return self.giam_gia
-    #def ghi_giam_gia (self, giam_gia_moi):
-    #    self.giam_gia = giam_gia_moi
+        self.__ten_san_pham = ten_san_pham
+        self.__gia = gia
+        self.__giam_gia = giam_gia
+    def get_ten_sp (self):
+        return self.__ten_san_pham
+    def set_ten_sp (self, ten_san_pham_moi):
+        self.__ten_san_pham = ten_san_pham_moi
+    def get_gia_sp (self):
+        return self.__gia
+    def set_gia_sp (self, gia_moi):
+        self.__gia = gia_moi
+    def get_giam_gia (self):
+        return self.__giam_gia
+    def set_giam_gia (self, giam_gia_moi):
+         self.__giam_gia = giam_gia_moi
     def thue_nhap_khau (self):
-        return self.gia * 0.1
+        return self.__gia * 0.1
     def nhap_thong_tin_san_pham(self):
-        self.ten_san_pham = input('Nhập tên sản phẩm: ')
-        self.gia = float(input('Nhập giá sản phẩm: '))
-        self.giam_gia = float(input('Nhập giảm giá sản phẩm: '))
+        self.set_ten_sp(input('Nhập tên sản phẩm: '))
+        self.set_gia_sp(float(input('Nhập giá sản phẩm: ')))
+        self.set_giam_gia(float(input('Nhập giảm giá sản phẩm: ')))
     def xuat_thong_tin_san_pham(self):
-        print(f'Tên sản phẩm: {self.ten_san_pham}')
-        print(f'Có giá: {self.gia}')
-        print(f'Được giảm giá: {self.giam_gia}')
+        print(f'Tên sản phẩm: {self.get_ten_sp()}')
+        print(f'Có giá: {self.get_gia_sp()}')
+        print(f'Được giảm giá: {self.get_giam_gia()}')
         print(f'Thuế nhập khẩu: {self.thue_nhap_khau()}')
     def __str__(self):
-        return (f'Tên sản phẩm: {self.ten_san_pham}\n'
-                f'Có giá: {self.gia}\n'
-                f'Được giảm giá: {self.giam_gia}\n'
+        return (f'Tên sản phẩm: {self.get_ten_sp()}\n'
+                f'Có giá: {self.get_gia_sp()}\n'
+                f'Được giảm giá: {self.get_giam_gia()}\n'
                 f'Thuế nhập khẩu: {self.thue_nhap_khau()}')
 
 
